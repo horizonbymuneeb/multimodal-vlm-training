4d872d68bd916eb91a4285caa71082fcd69e2e47
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-10-03 14:30:28 +0500

    Add LoRA layer to vision encoder for efficiency

--- src/main.py
+++ src/main.py
@@ -226,3 +226,5 @@ if __name__ == '__main__':
 # Optimize mixed-precision training stability [2025-09-30T14:19:20]
 
 # Add early stopping with configurable patience [2025-10-03T18:12:54]
+
+# Add LoRA layer to vision encoder for efficiency [2025-10-03T14:30:28]
