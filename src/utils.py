f1e62727453b2d55f998d730de37fd7cc3b32fc6
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-11-05 11:52:26 +0500

    Add LoRA layer to vision encoder for efficiency

--- src/utils.py
+++ src/utils.py
@@ -121,3 +121,5 @@ def format_number(n: int) -> str:
 # Add data augmentation pipeline for images [2025-10-28T10:54:06]
 
 # Fix batch norm sync across distributed GPUs [2025-11-04T09:11:11]
+
+# Add LoRA layer to vision encoder for efficiency [2025-11-05T11:52:26]
