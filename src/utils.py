f6b19c6812852e663e682dcf7ead8d11a8ca8fa6
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-01-15 13:14:18 +0500

    Add LoRA layer to vision encoder for efficiency

--- src/utils.py
+++ src/utils.py
@@ -169,3 +169,5 @@ def format_number(n: int) -> str:
 # Profile memory with activation checkpointing [2026-01-12T11:04:03]
 
 # Implement custom contrastive loss function [2026-01-15T19:32:29]
+
+# Add LoRA layer to vision encoder for efficiency [2026-01-15T13:14:18]
